2d70d6f8f0338f3b74dbe291f0b18ba64b6926ac
jamesarems <jamesarems@gmail.com>
2016-06-13 15:37:26 +0400

    updates

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ service iptables stop
 chkconfig iptables off
 mkdir /var/www/html/generator
 mkdir /var/www/html/client
+cp $LOC/server /usr/bin/server
+chmod +x /usr/bin/server
 touch /var/www/html/client/users.txt
 cp $LOC/gen.sh /var/www/html/generator/
 chmod +x /var/www/html/generator/gen.sh
