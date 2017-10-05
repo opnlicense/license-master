af18c284eaae8fdd0136f5c7f80aa3d82c2d9b65
jamesarems <jamesarems@gmail.com>
2017-10-05 14:42:50 +0400

    update

--- gen.sh
+++ gen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-##License generator 
+##License generator
 n=$RANDOM
 read -p "Enter 6 numbers:" a
 read -p "Licensee Name:" b
 md5="$a$n"
-hash="$(echo -n "$md5" | md5sum )"
+hash="$(echo -n "$md5" | md5sum | cut -c1-32 )"
 echo "$hash"
 touch /var/www/html/$hash
 echo "licensed" > /var/www/html/$hash
